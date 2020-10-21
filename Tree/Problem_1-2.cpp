06d0fdb0ce7b6e0ccc663bf5f2565d2c70318e66
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2020-10-22 03:28:06 +0900

    .

---
+++ Tree/Problem_1-2.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node* parent;
+	vector<Node*> children;
+	Node(int data)
+	{
+		this->data = data;
+		this->parent = nullptr;
+	}
+	~Node() {}
+
+	void setParent(Node* parent)
+	{
+		this->parent = parent;
+	}
+	void insertChild(Node* child)
+	{
+		this->children.push_back(child);
+	}
+	void delChild(Node* child)
+	{
+		for (unsigned int i = 0; i < this->children.size(); i++)
+		{
+			if (this->children[i] == child)
+				this->children.erase(this->children.begin() + i);
+		}
+	}
+};
+
+class Tree
+{
+private:
+	Node* root;
+	vector<Node*> node_list;
+public:
+
+	Tree()
+	{
+		root = new Node(1);
+		node_list.push_back(root);
+	}
+	~Tree() {}
+	int size()
+	{
+		return node_list.size();
+	}
+	void insertNode(int par_data, int data)
+	{
+		for (int i = 0; i < size(); i++)
+		{
+			if (node_list[i]->data == par_data)
+			{
+				Node* node = new Node(data);
+				node->setParent(node_list[i]);
+				node_list[i]->insertChild(node);
+				node_list.push_back(node);
+				return;
+			}
+		}
+	}
+	void delNode(int data)
+	{
+		Node* nownode;
+		Node* par;
+		for (int i = 0; i < size(); i++)
+		{
+			if (this->node_list[i]->data == data)
+			{
+				nownode = node_list[i];
+				if (nownode == root)
+				{
+					return;
+				}
+				par = nownode->parent;
+				for (Node*& child : nownode->children)
+				{
+					par->insertChild(child);
+					child->parent = par;
+				}
+				par->delChild(nownode);
+				this->node_list.erase(this->node_list.begin() + i);
+				delete nownode;
+			}
+		}
+		return;
+	}
+	void printChild(int data)
+	{
+		for (int i = 0; i < size(); i++)
+		{
+			if (node_list[i]->data == data)
+			{
+				if (node_list[i]->children.size() == 0)
+				{
+					cout << "0" << endl;
+					return;
+				}
+				else
+				{
+					for (unsigned int j = 0; j < node_list[i]->children.size(); j++)
+					{
+						cout << node_list[i]->children[j]->data << " ";
+					}
+					cout << endl;
+					return;
+				}
+			}
+		}
+	}
+	void printSibling(int data)
+	{
+		for (int i = 0; i < size(); i++)
+		{
+			if (node_list[i]->data == data)
+			{
+				if (node_list[i]->parent == nullptr)
+				{
+					cout << data << endl;
+					return;
+				}
+				else
+				{
+					Node* par_node = node_list[i]->parent;
+					for (unsigned j = 0; j < par_node->children.size(); j++)
+					{
+						cout << par_node->children[j]->data << " ";
+					}
+					cout << endl;
+					return;
+				}
+			}
+		}
+		cout << 0 << endl;
+	}
+};
+
+int main()
+{
+	int N;
+	cin >> N;
+	Tree* tree = new Tree();
+	for (int i = 0; i < N; i++)
+	{
+		string operation;
+		cin >> operation;
+		if (operation == "insert")
+		{
+			int par, data;
+			cin >> par >> data;
+			tree->insertNode(par, data);
+		}
+		else if (operation == "delete")
+		{
+			int data;
+			cin >> data;
+			tree->delNode(data);
+		}
+		else if (operation == "print")
+		{
+			int data;
+			cin >> data;
+			tree->printChild(data);
+		}
+	}
+}
